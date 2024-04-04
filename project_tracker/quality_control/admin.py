b93e4434d682e841e31ea1f95158b177ea3238ba
Adonin <adoninaa0@mail.ru>
2024-04-04 19:02:39 +0300

    first version

---
+++ project_tracker/quality_control/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import BugReport, FeatureRequest
+
+# Класс администратора для модели BugReport
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'update_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    ordering = ('created_at',)
+
+# Класс администратора для модели FeatureRequest
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'update_at')
+    list_filter = ('status',  'project')
+    search_fields = ('title', 'description')
+    ordering = ('created_at',)
